0e51943e0d481aecdff953eb00cfaf5e175efca9
MohammadJavad akbari <javadakbari1379@gmail.com>
2019-07-30 17:56:22 +0430

    add public and private trip view

--- carpooling/trip/urls.py
+++ carpooling/trip/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from trip.views import TripHandler
 app_name = "trip"
 urlpatterns = [
+    path('public/', TripHandler.handle_public_trip, name='public-trips'),
+    path('group/', TripHandler.handle_group_trips, name='group-trips'),
+    path('group/<int:id>/', TripHandler.handle_group_trips, name='trips-by-group'),
+    #path('all/')
 ]
+
--- carpooling/trip/views.py
+++ carpooling/trip/views.py
@@ -1,3 +1,30 @@
+from django.db.models import Q
+from django.http import HttpResponseNotFound, HttpResponseForbidden
 from django.shortcuts import render
-
+from trip.models import Trip
+from group.models import Group
 # Create your views here.
+
+
+class TripHandler:
+    @staticmethod
+    def handle_public_trip(request):
+        trips = Trip.objects.filter(Q(is_private=False))
+        return render(request, 'show_trips.html', {'trips': trips})
+
+    @staticmethod
+    def handle_group_trips(request, group_id):
+        if request.method == 'GET':
+            try:
+                group = Group.objects.get(id=group_id)
+            except Group.DoesNotExist:
+                return HttpResponseNotFound()
+
+            if not group.is_private:
+                return render(request, 'show_trips.html', {'trips': group.trip_set})
+
+            if request.user.is_anonymous or not request.user.is_authenticated():
+                return HttpResponseForbidden()
+
+            if group.members.get(username=request.user.username):
+                return render(request, 'show_trips.html', {'trips': group.trip_set})
