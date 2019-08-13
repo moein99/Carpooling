9c837b5e3af3e1356bc10cade75044cfbfa2ebb8
mohsenemne <mohsenemne@gmail.com>
2019-08-13 16:40:02 +0430

    Refactor get trip; Implement join trip tests; Assign login url with reverse lazy in setings.py
    Use function based views for small and simple views.

--- carpooling/root/decorators.py
+++ carpooling/root/decorators.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponseNotAllowed
+
+
 def check_request_type(original_function):
     def decorator(self, *args, **kwargs):
         type = self.request.POST.get('type', 'POST')
@@ -6,4 +9,13 @@ def check_request_type(original_function):
         elif type == 'DELETE':
             return self.delete(*args, **kwargs)
         return original_function(self, *args, **kwargs)
+    return decorator
+
+
+def only_get_allowed(original_function):
+    def decorator(request, *args, **kwargs):
+        if request.method == 'GET':
+            return original_function(request, *args, **kwargs)
+        else:
+            return HttpResponseNotAllowed()
     return decorator
