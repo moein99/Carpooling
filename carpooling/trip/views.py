f2c9bad16ab5bd86d35259a8c7fa82abead1230b
MohammadJavad akbari <javadakbari1379@gmail.com>
2019-08-14 10:25:09 +0430

    final search_trips

--- carpooling/trip/views.py
+++ carpooling/trip/views.py
@@ -2,27 +2,22 @@ import numpy as np
 from django.contrib.auth.decorators import login_required
 from django.contrib.gis.geos import Point
 from django.db.models import Q
-from django.http import HttpResponseNotFound, HttpResponseForbidden, HttpResponseNotAllowed
-from django.utils.decorators import method_decorator
-from django.views.generic.base import View
-from numpy.linalg import norm
-
-from group.models import Group, Membership
-from django.contrib.gis.geos import Point
-from django.http import HttpResponseBadRequest, HttpResponse
 from django.http import HttpResponseBadRequest
 from django.http import HttpResponseForbidden, HttpResponseNotAllowed
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
+from django.utils.termcolors import background
 from django.views.generic.base import View
 from expiringdict import ExpiringDict
 from geopy.distance import distance as point_distance
+from numpy.linalg import norm
 
 from carpooling.settings import DISTANCE_THRESHOLD
 from group.models import Group, Membership
 from trip.forms import TripForm
 from trip.models import Trip, TripGroups
+from trip.utils import extract_source, extract_destination
 
 user_groups_cache = ExpiringDict(max_len=100, max_age_seconds=5 * 60)
 
@@ -41,8 +36,8 @@ class TripCreationHandler(View):
 
     @staticmethod
     def create_trip(car_provider, post_data):
-        source = Point(float(post_data['source_lat']), float(post_data['source_lng']))
-        destination = Point(float(post_data['destination_lat']), float(post_data['destination_lng']))
+        source = extract_source(post_data)
+        destination = extract_destination(post_data)
         trip_form = TripForm(data=post_data)
         if trip_form.is_valid() and TripForm.is_point_valid(source) and TripForm.is_point_valid(destination):
             trip_obj = trip_form.save(commit=False)
@@ -186,12 +181,8 @@ class SearchTripsManger(View):
     @method_decorator(login_required)
     def get(self, request):
         if SearchTripsManger.is_valid_search_parameter(request.GET):
-            if request.GET:
-                return SearchTripsManger.do_search(request)
-            else:
-                return render(request, "search_trip")
-        else:
-            return HttpResponseBadRequest()
+            return SearchTripsManger.do_search(request) if request.GET else render(request, "search_trip")
+        return HttpResponseBadRequest()
 
     @staticmethod
     def subtract_two_point(point1: Point, point2: Point):
@@ -204,7 +195,7 @@ class SearchTripsManger(View):
         trip_source_to_destination = SearchTripsManger.subtract_two_point(query.destination, query.source)
 
         if np.dot(trip_source_to_destination, SearchTripsManger.subtract_two_point(destination, source)) < 0:
-            return 360 + 360
+            return np.inf
         else:
             if np.dot(source_to_trip_source, trip_source_to_destination) > 0:
                 # distance to source dot
@@ -226,11 +217,14 @@ class SearchTripsManger(View):
 
     @staticmethod
     def do_search(request):
-        post_data = request.GET
-        source = Point(float(post_data['source_lat']), float(post_data['source_lng']))
-        destination = Point(float(post_data['destination_lat']), float(post_data['destination_lng']))
-        trips = (request.user.driving_trips.all() | request.user.partaking_trips.all()).distinct().exclude(status=Trip.DONE_STATUS)
-        trips = sorted(trips, key=lambda query: (SearchTripsManger.search_sort(query, source=source, destination=destination)))[:5]
+        data = request.GET
+        source = Point(float(data['source_lat']), float(data['source_lng']))
+        destination = Point(float(data['destination_lat']), float(data['destination_lng']))
+        trips = (request.user.driving_trips.all() | request.user.partaking_trips.all()).distinct().exclude(
+            status=Trip.DONE_STATUS)
+        trips = sorted(trips, key=lambda query: (
+            SearchTripsManger.search_sort(query, source=source, destination=destination)))
+        trips = filter(lambda query: SearchTripsManger.search_sort(query, source, destination) != np.inf, trips)
         return render(request,
                       "trips_viewer.html",
                       {"trips": trips}
@@ -239,6 +233,7 @@ class SearchTripsManger(View):
     @staticmethod
     def is_valid_search_parameter(post_data):
         if post_data:
-            return 'source_lat' in post_data and 'source_lng' in post_data and 'destination_lat' in post_data and 'destination_lng' in post_data
+            return 'source_lat' in post_data and 'source_lng' in post_data and 'destination_lat' in post_data and \
+                   'destination_lng' in post_data
         else:
             return True
