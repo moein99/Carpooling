f4326e1c00ff7fc1900ee921c2ca592e875ea3ce
span <sepehr2012a@gmail.com>
2019-09-01 13:39:44 +0430

    add log for group view

--- carpooling/group/views.py
+++ carpooling/group/views.py
@@ -15,6 +15,7 @@ from group.forms import GroupForm
 from group.models import Group, Membership
 from group.utils import get_group_membership, add_to_group, join_group, manage_group_authorization
 from root.decorators import only_get_allowed, check_request_type
+
 log = logging.getLogger(__name__)
 
 
@@ -46,6 +47,7 @@ class CreateGroupManager(View):
             group.save()
             CreateGroupManager.index_goup(group, request)
             Membership.objects.create(member=request.user, group=group, role='ow')
+            log.info("user:{} made group:{}".format(request.user.id, group.id))
             return redirect(reverse('group:groups_list'))
         return render(request, "create_group.html", {
             "form": form,
@@ -71,6 +73,7 @@ class CreateGroupManager(View):
             "description": group.description
         }
         INDEX.index_group(data, schedule=1)
+        log.info("group:{} was added to elastic search".format(request.user.id, group.id))
 
 
 @login_required
@@ -105,13 +108,16 @@ class GroupManager(View):
         is_owner, has_joined = manage_group_authorization(membership)
         request_action = request.POST['action']
         if request_action == 'join' and not has_joined:
+            log.info("user:{} joined group:{}".format(request.user.id, group_id))
             errors.append(join_group(request.user, group))
             has_joined = True
         elif request_action == 'leave' and has_joined:
+            log.info("user:{} left group:{}".format(request.user.id, group_id))
             membership.delete()
             has_joined = False
             errors.append("you left the group")
         elif request_action == 'add' and is_owner:
+            log.info("user:{} added user:{} to group:{}".format(request.user.id, request.POST['username'], group_id))
             errors.append(add_to_group(request.POST['username'], group))
 
         return render(request, "manage_group.html", {
@@ -150,6 +156,7 @@ class GroupMembersManager(View):
         if group.is_private and membership is None:
             return HttpResponse("You are not authorized to remove a member", status=403)
         if membership and membership.role == 'ow':
+            log.info("user:{} removed user:{} from group:{}".format(request.user.id, member_id, group_id))
             get_object_or_404(Membership, group_id=group.id, member_id=member_id).delete()
             return redirect(reverse('group:group_members', kwargs={'group_id': group_id}))
         return HttpResponse("You are not authorized to remove a member", status=403)
@@ -159,6 +166,7 @@ class SearchGroupManager:
     @classmethod
     @method_decorator(login_required)
     def search_group_view(cls, request, query):
+        log.info("user {} searched for groups with containing:{}".format(request.user.id, query))
         result = {'groups': []}
         for group in search_query.group_search_with_out_map(query)['hits']['hits']:
 
@@ -174,17 +182,16 @@ class SearchGroupManager:
 
 
 @login_required
+@only_get_allowed
 def sort(request):
-    if request.method == "GET":
-        data = {
-            "lat": float(request.GET['source_lat']),
-            "lon": float(request.GET['source_lon'])
-        }
-        group_list = []
-        for group in search_query.group_search_with_map(data)['hits']['hits']:
-            instance = get_object_or_404(Group, id=group['_id'])
-            if not instance.is_private or Membership.objects.filter(member=request.user, group=instance).exists():
-                group_list.append(instance)
-        return render(request, "sorted_group_list.html", {"group_list": group_list})
-    else:
-        HttpResponse("Method Not Allowed", status=405)
+    data = {
+        "lat": float(request.GET['source_lat']),
+        "lon": float(request.GET['source_lon'])
+    }
+    log.info("user {} searched near by groups to lat:{} lon:{}".format(request.user.id, data["lat"], data["lon"]))
+    group_list = []
+    for group in search_query.group_search_with_map(data)['hits']['hits']:
+        instance = get_object_or_404(Group, id=group['_id'])
+        if not instance.is_private or Membership.objects.filter(member=request.user, group=instance).exists():
+            group_list.append(instance)
+    return render(request, "sorted_group_list.html", {"group_list": group_list})
